5c20860aa18ee289634a15d95293e748c02e688d
Molly Finley <molly@mlfinley.com>
2023-04-17 21:59:55 -0400

    Update data cleaning and formatting in script
    - Import re module for regex usage
    - Add cleaning methods for removing tags, links, and Wordle messages
    - Apply cleaning methods before splitting messages into prompts and responses
    - Filter out empty messages before splitting
    - Update dictionary keys in to_dict function

--- test/data_clean.py
+++ test/data_clean.py
@@ -1,32 +1,38 @@
 import csv
 from datetime import datetime
 import json
+import re
+
+filename = 'molly'
 
 def read_csv(filename):
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         return list(reader)
 
-'''
-convert str formatted 2023-01-13 21:21:11.750000+00:00 to datetime
-'''
 def to_time(str):
     str = str.split('+')[0].split('.')[0]
-    # print(str)
     return datetime.strptime(str, '%Y-%m-%d %H:%M:%S')
 
-data = read_csv('data/molly.csv')
+data = read_csv(f'data/{filename}.csv')
 data = [row[1:3] for row in data[1:]]
-data = [(to_time(timestamp),message) for timestamp, message in data]
+data = [(to_time(timestamp), message) for timestamp, message in data]
 
 data = [row for row in data if row[0].year >= 2022]
 
-'''
-turn string into prompt and response
+# Cleaning methods
+def remove_tags_links(s):
+    s = re.sub(r"<(@|#)[!]?[0-9]+>", "", s)
+    s = re.sub(r"(www|http[s]*)\S+\w?", "", s)
+    return s
 
-does this by splitting on spaces and taking the first half as the prompt
-and the remainder as the response
-'''
+def is_not_wordle_message(s):
+    return not re.match(r"Wordle [0-9]+.+", s)
+
+data = [(timestamp, remove_tags_links(message)) for timestamp, message in data]
+data = [row for row in data if is_not_wordle_message(row[1])]
+data = [row for row in data if len(row[1]) > 0]
+
 def to_prompt_response(message):
     message = message.split(' ')
     prompt = ' '.join(message[:len(message)//2])
@@ -37,11 +43,12 @@ data = [to_prompt_response(message) for _, message in data]
 
 def to_dict(prompt, response):
     return {
-        'prompt': prompt,
-        'completion': response
+        'instruction': 'Autocomplete',
+        'input': prompt,
+        'output': response
     }
 
 data = [to_dict(prompt, response) for prompt, response in data]
 
-with open('data/molly_data.json', 'w') as f:
+with open(f'data/{filename}.json', 'w') as f:
     json.dump(data, f)
